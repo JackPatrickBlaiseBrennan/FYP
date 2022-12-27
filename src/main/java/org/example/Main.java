fa1714b870ae68d63df2101cbba20bc16faac610
JackPatrickBlaiseBrennan <jackpbbrennan@gmail.com>
2022-12-27 21:04:21 +0000

    CP: Fix consec constraint, multiple solutions, update user constraints closes #1 closes #2 closes #3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
-import org.chocosolver.solver.variables.BoolVar;
+
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.util.tools.ArrayUtils;
 
@@ -26,6 +26,7 @@ public class Main {
         int numSubPeriods = (int) Math.ceil((double)period / 4);
         int subPeriodLength = (int) Math.ceil((double)period / numSubPeriods);
 
+        IntVar numberOfGranted = model.intVar("numberOfGranted", 0, numEmployees*period);
         IntVar[][] roster = model.intVarMatrix("roster", period, numEmployees, 0, numShifts);
 
         // Constraints
@@ -49,36 +50,46 @@ public class Main {
                 if (j < period - 1){
                     // no Morning after Late
                     roster[j][i].eq(3).imp(roster[j+1][i].ne(1)).post();
-                    // cannot work on the same shift for more than two days
-                    if (j % 2 == 0){
-                        allDifferentExcept0(model, roster[j][i],roster[j+1][i]);
+                    // cannot work on the same shift for more than three days
+                    if (j < period - 3){
+                        roster[j][i].eq(roster[j+1][i]).eq(roster[j+2][i]).imp(roster[j+3][i].ne(roster[j+1][i])).post();
                     }
                 }
             }
         }
 
-        // user constraints
-        BoolVar dayoff = model.arithm(roster[3][0], "=", 0).reify();
-        BoolVar dayoff2 = model.arithm(roster[3][1], "=", 0).reify();
-        BoolVar dayoff3 = model.arithm(roster[3][2], "=", 0).reify();
+        IntVar[] dayOffGranted = model.intVarArray("dayOffGranted", 7, 0, 1);
+
+//        for (int i = 0; i < 3; i++){
+//            model.ifOnlyIf(model.arithm(dayOffGranted[i], ">", 0), model.arithm(roster[3][i], "=", 0));
+//        }
+        model.ifOnlyIf(model.arithm(dayOffGranted[0], ">", 0), model.arithm(roster[3][0], "=", 0));
+        model.ifOnlyIf(model.arithm(dayOffGranted[1], ">", 0), model.arithm(roster[3][1], "=", 0));
+        model.ifOnlyIf(model.arithm(dayOffGranted[2], ">", 0), model.arithm(roster[3][2], "=", 0));
+        model.ifOnlyIf(model.arithm(dayOffGranted[3], ">", 0), model.arithm(roster[3][3], "=", 0));
+        model.ifOnlyIf(model.arithm(dayOffGranted[4], ">", 0), model.arithm(roster[3][4], "=", 0));
+        model.ifOnlyIf(model.arithm(dayOffGranted[5], ">", 0), model.arithm(roster[3][5], "=", 0));
+        model.ifOnlyIf(model.arithm(dayOffGranted[6], ">", 0), model.arithm(roster[3][6], "=", 0));
+        model.sum(dayOffGranted,"=",numberOfGranted).post();
+
+        model.setObjective(Model.MAXIMIZE, numberOfGranted);
 
         Solver s = model.getSolver();
-        s.showSolutions();
-        s.solve();
+        s.limitTime("2s");
+//        s.showSolutions();
 
-        for (int j = 0; j < numEmployees; j++){
-            System.out.print("Employee" + (j+1)+": ");
-            for (int i =0; i < period; i++){
-                System.out.print(shifts[roster[i][j].getValue()] + " ");
+        while (s.solve()) { //print the solution
+            System.out.println("Solution " + s.getSolutionCount() + ":");
+            for (int j = 0; j < numEmployees; j++) {
+                System.out.print("Employee" + (j + 1) + ": ");
+                for (int i = 0; i < period; i++) {
+                    System.out.print(shifts[roster[i][j].getValue()] + " ");
+                }
+                System.out.print("\n");
             }
-            System.out.print("\n");
+            System.out.print("Number of user requests granted: " + numberOfGranted.getValue() + "\n");
         }
-        System.out.print(dayoff);
-        System.out.print(dayoff2);
-        System.out.print(dayoff3);
+        s.printStatistics();
     }
 
-    public static void allDifferentExcept0(Model model, IntVar... vars){
-        model.allDifferentExcept0(vars).post();
-    }
 }
